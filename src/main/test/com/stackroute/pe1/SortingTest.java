e50d48a9cf434d8a79cc2b685172c9c677e1d334
isbaska <isbaska1@in.ibm.com>
2019-06-27 16:11:35 +0530

    Sorting class issue is fixed

--- src/main/test/com/stackroute/pe1/SortingTest.java
+++ src/main/test/com/stackroute/pe1/SortingTest.java
@@ -9,7 +9,7 @@ public class SortingTest {
 	Sorting sorting = new Sorting();
 	
 	@Test
-	public void sortPositiveInteger(){
+	public void sortInteger(){
 
 //		Act
 		int result = sorting.sort(73914);
@@ -19,25 +19,15 @@ public class SortingTest {
 		assertEquals("TestPassed",97431, result);
 	}
 
-	@Test
-	public void sortNegativeInteger(){
-
-//		Act
-		int result = sorting.sort(-47292);
-		System.out.println("Test2");
-
-//  	Assert
-		assertEquals("TestPassed","Error:Please enter a positive integer", result);
-	}
 	
 	@Test
-	public void sortPositiveIntegerWhichContainsZero(){
+	public void sortIntegerWhichContainsZero(){
 
 //		Act
 		int result = sorting.sort(43024);
 		System.out.println("Test3");
 
 //  	Assert
-		assertEquals("TestPassed",44200, result);
+		assertEquals("TestPassed",44320, result);
 	}
 }
